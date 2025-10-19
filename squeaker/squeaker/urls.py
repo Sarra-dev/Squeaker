4a6e1a30571181d20b5561bff8bef2806dbd7606
FediAYADI <ayadifedi666@gmail.com>
2025-10-20 00:19:40 +0100

    Update user profile + profile puc + upload profile pic with form + add profile pic to tweets

--- squeaker/squeaker/urls.py
+++ squeaker/squeaker/urls.py
@@ -1,7 +1,12 @@
+
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('musker.urls')),
-]
+    path(f"{settings.ADMIN_URL}/", admin.site.urls),
+    path("", include("musker.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
